94aa86c1a8c253fba8e7136602ed8d524ba75ceb
Richard McGinn <richard.mcginn@blakyaks.com>
2025-06-17 16:31:35 +0100

    Set always_on to correct value for shared tier

--- terraform/web-app.tf
+++ terraform/web-app.tf
@@ -1,20 +1,22 @@
 resource "azurerm_resource_group" "rg-web-app" {
-  name = "rg-web-app"
+  name     = "rg-web-app"
   location = "uksouth"
 }
 
 resource "azurerm_service_plan" "lab-service-plan" {
-  name = "lab-service-plan"
-  location = azurerm_resource_group.rg-web-app.location
+  name                = "lab-service-plan"
+  location            = azurerm_resource_group.rg-web-app.location
   resource_group_name = azurerm_resource_group.rg-web-app.name
-  os_type = "Windows"
-  sku_name = "D1"
+  os_type             = "Windows"
+  sku_name            = "D1"
 }
 
 resource "azurerm_windows_web_app" "lab-app" {
-  name = "lab-app"
-  location = azurerm_resource_group.rg-web-app.location
+  name                = "lab-app"
+  location            = azurerm_resource_group.rg-web-app.location
   resource_group_name = azurerm_resource_group.rg-web-app.name
-  service_plan_id = azurerm_service_plan.lab-service-plan.id
-  site_config {}
+  service_plan_id     = azurerm_service_plan.lab-service-plan.id
+  site_config {
+    always_on = false
+  }
 }
