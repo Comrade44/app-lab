bb0d78bad838ae50555d0eb2d4a4ec5f2e3f5cb3
Richard McGinn <richard.mcginn@blakyaks.com>
2025-06-19 10:21:12 +0100

    Add DNS zones

--- terraform/network.tf
+++ terraform/network.tf
@@ -16,3 +16,17 @@ resource "azurerm_subnet" "vnet-01-snet-app" {
   resource_group_name  = azurerm_resource_group.rg-network.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_dns_zone" "dns-zones" {
+  for_each            = ["privatelink.database.windows.net", "privatelink.azurewebsites.net", "scm.privatelink.azurewebsites.net"]
+  name                = each.value
+  resource_group_name = azurerm_resource_group.rg-network.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "link-zones" {
+  for_each              = azurerm_dns_zone.dns-zones
+  name                  = "link-${each.value.name}"
+  resource_group_name   = azurerm_resource_group.rg-network.name
+  private_dns_zone_name = each.value.name
+  virtual_network_id    = azurerm_virtual_network.vnet-01.id
+}
