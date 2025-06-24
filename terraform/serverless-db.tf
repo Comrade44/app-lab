4c25aae2afc317a8fec3ee2bdddc4f8a69263d97
Richard McGinn <richard.mcginn@blakyaks.com>
2025-06-24 12:08:50 +0100

    Add serverless-db

---
+++ terraform/serverless-db.tf
@@ -0,0 +1,7 @@
+resource "azurerm_mssql_database" "serverless" {
+  name = "serverless"
+  server_id = azurerm_mssql_server.sql-server.id
+  auto_pause_delay_in_minutes = 5
+  sku_name = "GP_S_Gen5_1"
+  min_capacity = 0.5
+}
