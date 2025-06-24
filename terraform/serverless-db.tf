0568f74364e3d8cc2f78fc4006d352d2dfe1cf7b
Richard McGinn <richard.mcginn@blakyaks.com>
2025-06-24 12:11:42 +0100

    Fix parameter with correct min value

--- terraform/serverless-db.tf
+++ terraform/serverless-db.tf
@@ -1,7 +1,7 @@
 resource "azurerm_mssql_database" "serverless" {
   name = "serverless"
   server_id = azurerm_mssql_server.sql-server.id
-  auto_pause_delay_in_minutes = 5
+  auto_pause_delay_in_minutes = 15
   sku_name = "GP_S_Gen5_1"
   min_capacity = 0.5
 }
