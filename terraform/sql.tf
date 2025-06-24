1abeb81c3acf246d5012b0a3a9306472fef17b91
Richard McGinn <richard.mcginn@blakyaks.com>
2025-06-24 11:44:17 +0100

    SQL server setup

--- terraform/sql.tf
+++ terraform/sql.tf
@@ -12,11 +12,11 @@ resource "azurerm_mssql_server" "sql-server" {
   administrator_login_password = "LabLogin2025!"
 }
 
-resource "azurerm_mssql_database" "sql-db-01" {
-  name        = "sql-db-01"
-  server_id   = azurerm_mssql_server.sql-server.id
-  sample_name = "AdventureWorksLT"
-}
+#resource "azurerm_mssql_database" "sql-db-01" {
+#  name        = "sql-db-01"
+#  server_id   = azurerm_mssql_server.sql-server.id
+#  sample_name = "AdventureWorksLT"
+#}
 
 resource "azurerm_mssql_firewall_rule" "allow-ms-trusted-services" {
   name             = "AllowAllWindowsAzureIps"
@@ -25,20 +25,20 @@ resource "azurerm_mssql_firewall_rule" "allow-ms-trusted-services" {
   end_ip_address   = "0.0.0.0"
 }
 
-resource "azurerm_private_endpoint" "sql" {
-  name                = "${azurerm_mssql_database.sql-db-01.name}-PEP"
-  location            = azurerm_resource_group.rg-network.location
-  resource_group_name = azurerm_resource_group.rg-network.name
-  subnet_id           = azurerm_subnet.vnet-01-snet-app.id
-
-  private_service_connection {
-    name                           = "sql-psc"
-    private_connection_resource_id = azurerm_mssql_server.sql-server.id
-    subresource_names              = ["sqlServer"]
-    is_manual_connection           = false
-  }
-  private_dns_zone_group {
-    name                 = "dns-zone-group"
-    private_dns_zone_ids = [azurerm_private_dns_zone.dns-zones["privatelink.database.windows.net"].id]
-  }
-}
+#resource "azurerm_private_endpoint" "sql" {
+#  name                = "${azurerm_mssql_database.sql-db-01.name}-PEP"
+#  location            = azurerm_resource_group.rg-network.location
+#  resource_group_name = azurerm_resource_group.rg-network.name
+#  subnet_id           = azurerm_subnet.vnet-01-snet-app.id
+#
+#  private_service_connection {
+#    name                           = "sql-psc"
+#    private_connection_resource_id = azurerm_mssql_server.sql-server.id
+#    subresource_names              = ["sqlServer"]
+#    is_manual_connection           = false
+#  }
+#  private_dns_zone_group {
+#    name                 = "dns-zone-group"
+#    private_dns_zone_ids = [azurerm_private_dns_zone.dns-zones["privatelink.database.windows.net"].id]
+#  }
+#}
