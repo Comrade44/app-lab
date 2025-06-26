8b0f75e68fca96e04f2ed12b417bebffb8a64c89
Richard McGinn <richard.mcginn@blakyaks.com>
2025-06-26 15:54:16 +0100

    Add subnet delegation

--- terraform/sql-mi.tf
+++ terraform/sql-mi.tf
@@ -16,7 +16,7 @@ resource "azurerm_mssql_managed_instance" "sql-mi" {
   resource_group_name          = azurerm_resource_group.rg-sql.name
   sku_name                     = "GP_Gen5"
   storage_size_in_gb           = 32
-  subnet_id                    = azurerm_subnet.vnet-01-snet-app.id
+  subnet_id                    = azurerm_subnet.sql-mi.id
   vcores                       = 4
   administrator_login          = "labadmin"
   administrator_login_password = "LabLogin2025!"
@@ -27,4 +27,24 @@ resource "azurerm_mssql_firewall_rule" "allow-ms-trusted-services" {
   server_id        = azurerm_mssql_managed_instance.sql-mi.id
   start_ip_address = "0.0.0.0"
   end_ip_address   = "0.0.0.0"
+}
+
+resource "azurerm_subnet" "sql-mi" {
+  name                 = "sql-mi"
+  resource_group_name  = azurerm_resource_group.rg-network.name
+  virtual_network_name = azurerm_virtual_network.vnet-01.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+
+  delegation {
+    name = "sql-mi"
+    service_delegation {
+      name = "Microsoft.Sql/managedInstances"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+        "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
+        "Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action"
+      ]
+    }
+  }
 }
