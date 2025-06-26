3720e54c455455f1489e152fe55300f430a23ddd
Richard McGinn <richard.mcginn@blakyaks.com>
2025-06-26 15:31:50 +0100

    Username and password

--- terraform/sql-mi.tf
+++ terraform/sql-mi.tf
@@ -10,14 +10,16 @@ resource "random_string" "web-app-name" {
 }
 
 resource "azurerm_mssql_managed_instance" "sql-mi" {
-  name                = "sql-mi"
-  license_type        = "LicenseIncluded"
-  location            = azurerm_resource_group.rg-sql.location
-  resource_group_name = azurerm_resource_group.rg-sql.name
-  sku_name            = "GP_Gen5"
-  storage_size_in_gb  = 32
-  subnet_id           = azurerm_subnet.vnet-01-snet-app.id
-  vcores              = 4
+  name                         = "sql-mi"
+  license_type                 = "LicenseIncluded"
+  location                     = azurerm_resource_group.rg-sql.location
+  resource_group_name          = azurerm_resource_group.rg-sql.name
+  sku_name                     = "GP_Gen5"
+  storage_size_in_gb           = 32
+  subnet_id                    = azurerm_subnet.vnet-01-snet-app.id
+  vcores                       = 4
+  administrator_login          = "labadmin"
+  administrator_login_password = "LabLogin2025!"
 }
 
 resource "azurerm_mssql_firewall_rule" "allow-ms-trusted-services" {
